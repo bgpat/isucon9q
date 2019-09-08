473250a37e5723f4c7b90b60b9650aa31119a733
bgpat <bgpat@users.noreply.github.com>
2019-09-08 14:02:38 +0900

    Add updateItemStatus

--- home/isucon/isucari/webapp/go/redis.go
+++ home/isucon/isucari/webapp/go/redis.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/go-redis/redis"
+	"github.com/pkg/errors"
 )
 
 var redisCli = redis.NewClient(&redis.Options{
@@ -14,4 +15,18 @@ var redisCli = redis.NewClient(&redis.Options{
 
 func itemsKey(status string) string {
 	return "items_" + status
+}
+
+func updateItemStatus(item Item, newStatus string) error {
+	if err := redisCli.ZRem(itemsKey(item.Status), item.ID).Err(); err != nil {
+		return errors.WithStack(err)
+	}
+	z := redis.Z{
+		Score:  float64(item.CreatedAt.Unix()) + float64(item.CreatedAt.UnixNano())*1e-18,
+		Member: item.ID,
+	}
+	if err := redisCli.ZAdd(itemsKey(item.Status), z).Err(); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
 }
