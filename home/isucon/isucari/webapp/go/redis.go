e2ea3d1c26a55ee20c22733dab0bad874a55c442
bgpat <bgpat@users.noreply.github.com>
2019-09-08 12:21:06 +0900

    Get redis addr from env

--- home/isucon/isucari/webapp/go/redis.go
+++ home/isucon/isucari/webapp/go/redis.go
@@ -5,7 +5,7 @@ import (
 )
 
 var redisCli = redis.NewClient(&redis.Options{
-	Addr:     "isucon9q-2:6379",
+	Addr:     os.Getenv("REDIS_ADDR"),
 	Password: "", // no password set
 	DB:       0,  // use default DB
 })
