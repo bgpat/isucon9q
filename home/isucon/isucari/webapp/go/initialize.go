a453e370d16e5abd60f51d81b2c238318f8f046d
bgpat <bgpat@users.noreply.github.com>
2019-09-08 12:00:59 +0900

    Extract initialize

---
+++ home/isucon/isucari/webapp/go/initialize.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func postInitialize(w http.ResponseWriter, r *http.Request) {
+	ri := reqInitialize{}
+
+	err := json.NewDecoder(r.Body).Decode(&ri)
+	if err != nil {
+		outputErrorMsg(w, http.StatusBadRequest, "json decode error")
+		return
+	}
+
+	cmd := exec.Command("../sql/init.sh")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stderr
+	cmd.Run()
+	if err != nil {
+		outputErrorMsg(w, http.StatusInternalServerError, "exec init.sh error")
+		return
+	}
+
+	_, err = dbx.Exec(
+		"INSERT INTO `configs` (`name`, `val`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `val` = VALUES(`val`)",
+		"payment_service_url",
+		ri.PaymentServiceURL,
+	)
+	if err != nil {
+		log.Print(err)
+		outputErrorMsg(w, http.StatusInternalServerError, "db error")
+		return
+	}
+	_, err = dbx.Exec(
+		"INSERT INTO `configs` (`name`, `val`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `val` = VALUES(`val`)",
+		"shipment_service_url",
+		ri.ShipmentServiceURL,
+	)
+	if err != nil {
+		log.Print(err)
+		outputErrorMsg(w, http.StatusInternalServerError, "db error")
+		return
+	}
+
+	res := resInitialize{
+		// キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
+		Campaign: 0,
+		// 実装言語を返す
+		Language: "Go",
+	}
+
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	json.NewEncoder(w).Encode(res)
+}
--- home/isucon/isucari/webapp/go/main.go
+++ home/isucon/isucari/webapp/go/main.go
@@ -10,7 +10,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strconv"
 	"time"
@@ -450,56 +449,6 @@ func getShipmentServiceURL() string {
 
 func getIndex(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "index.html", struct{}{})
-}
-
-func postInitialize(w http.ResponseWriter, r *http.Request) {
-	ri := reqInitialize{}
-
-	err := json.NewDecoder(r.Body).Decode(&ri)
-	if err != nil {
-		outputErrorMsg(w, http.StatusBadRequest, "json decode error")
-		return
-	}
-
-	cmd := exec.Command("../sql/init.sh")
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stderr
-	cmd.Run()
-	if err != nil {
-		outputErrorMsg(w, http.StatusInternalServerError, "exec init.sh error")
-		return
-	}
-
-	_, err = dbx.Exec(
-		"INSERT INTO `configs` (`name`, `val`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `val` = VALUES(`val`)",
-		"payment_service_url",
-		ri.PaymentServiceURL,
-	)
-	if err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-		return
-	}
-	_, err = dbx.Exec(
-		"INSERT INTO `configs` (`name`, `val`) VALUES (?, ?) ON DUPLICATE KEY UPDATE `val` = VALUES(`val`)",
-		"shipment_service_url",
-		ri.ShipmentServiceURL,
-	)
-	if err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-		return
-	}
-
-	res := resInitialize{
-		// キャンペーン実施時には還元率の設定を返す。詳しくはマニュアルを参照のこと。
-		Campaign: 0,
-		// 実装言語を返す
-		Language: "Go",
-	}
-
-	w.Header().Set("Content-Type", "application/json;charset=utf-8")
-	json.NewEncoder(w).Encode(res)
 }
 
 func getNewCategoryItems(w http.ResponseWriter, r *http.Request) {
