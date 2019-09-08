3253fe907595ad1b3fb1a06e834cde250b892563
Goryudyuma <umaretekyoumade@gmail.com>
2019-09-08 15:05:21 +0900

    redisから見るようにした

--- home/isucon/isucari/webapp/go/new_items.go
+++ home/isucon/isucari/webapp/go/new_items.go
@@ -34,28 +34,15 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 	items := []Item{}
 	if itemID > 0 && createdAt > 0 {
 		// paging
-		err := dbx.Select(&items,
-			"SELECT * FROM `items` WHERE `status` IN (?,?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
-			ItemStatusOnSale,
-			ItemStatusSoldOut,
-			time.Unix(createdAt, 0),
-			time.Unix(createdAt, 0),
-			itemID,
-			ItemsPerPage+1,
-		)
+		items, err = getItems([]string{ItemStatusOnSale, ItemStatusSoldOut}, time.Unix(createdAt, 0), ItemsPerPage+1)
 		if err != nil {
 			log.Print(err)
 			outputErrorMsg(w, http.StatusInternalServerError, "db error")
 			return
 		}
 	} else {
+		items, err = getItems([]string{ItemStatusOnSale, ItemStatusSoldOut}, time.Unix(createdAt, 0), ItemsPerPage+1)
 		// 1st page
-		err := dbx.Select(&items,
-			"SELECT * FROM `items` WHERE `status` IN (?,?) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
-			ItemStatusOnSale,
-			ItemStatusSoldOut,
-			ItemsPerPage+1,
-		)
 		if err != nil {
 			log.Print(err)
 			outputErrorMsg(w, http.StatusInternalServerError, "db error")
